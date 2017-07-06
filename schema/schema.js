50d7c68ca74bdc566b90e72543b09b01ea96e836
Jeff Kranking <jkranking@gmail.com>
2017-07-05 19:09:39 -0700

    adds rootQuery and resolve function to find user

--- schema/schema.js
+++ schema/schema.js
@@ -1,12 +1,18 @@
 const graphql = require('graphql');
+const _ = require('lodash'); // helper library that helps walk through collections of data (basically active record)
 const {
 	GraphQLObjectType,
 	GraphQLString,
 	GraphQLInt
 } = graphql;
 
+const users = [
+	{ id: '23', firstName: 'Bill', age: 20},
+	{ id: '47', firstName: 'Samantha', age: 21}
+]
+
 // instructs graphql on what a user object looks like
-const userType = new GraphQLObjectType({
+const UserType = new GraphQLObjectType({
 	name: 'User',
 	fields: {
 		id: { type: GraphQLString },
@@ -14,3 +20,23 @@ const userType = new GraphQLObjectType({
 		age: { type: GraphQLInt }
 	}
 });
+
+const RootQuery = new GraphQLObjectType({
+	name: 'RootQueryType',
+	fields: {
+		user: {
+			type: UserType,
+			args: {id: {type: GraphQLString }},
+			resolve(parentValue, args) {
+				// just working with hard coded data for now defined up top- no db
+				return _.find(users, { id: args.id })
+
+			}
+		}
+	}
+})
+
+// resolve function is used to go into db and get data you're looking for
+// everything else is definging the type of data.
+// parentValue is worthless - just go with it
+// args is important
